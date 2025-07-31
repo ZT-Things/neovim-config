48ea694651e8828f575715568ff035039a00364f
ZT <ztthingsgm@gmail.com>
2025-07-31 20:31:55 +0700

    Some more toggleterm remap

--- lua/zt/lazy/toggleterm.lua
+++ lua/zt/lazy/toggleterm.lua
@@ -1,6 +1,6 @@
 return {
     "akinsho/toggleterm.nvim",
-    version = "*", -- or use a specific tag like "v2.*"
+    version = "*",
     config = function()
         require("toggleterm").setup({
             size = 20,
@@ -28,11 +28,22 @@ return {
         end
 
         vim.keymap.set("n", "<leader>gg", _lazygit_toggle, { noremap = true, silent = true })
-        vim.keymap.set("n", "<C-t>", "<cmd>ToggleTerm<CR>", { noremap = true, silent = true })
+        -- vim.keymap.set("n", "<C-t>", "<cmd>ToggleTerm<CR>")
+        vim.keymap.set("n", "<C-t>", function()
+            vim.cmd("ToggleTerm")
+            vim.defer_fn(function()
+                local mode = vim.api.nvim_get_mode().mode
+                if mode ~= "i" and mode ~= "t" then
+                    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("a", true, false, true), "n", false)
+                end
+            end, 20)
+        end, { noremap = true,silent = true })
         vim.keymap.set("t", "<C-t>", "<C-\\><C-n><cmd>ToggleTerm<CR>", { noremap = true, silent = true })
-        vim.cmd [[
-          autocmd TermOpen * startinsert
-        ]]
+        vim.keymap.set("t", "<A-c>", "<C-\\><C-N>")
+        vim.keymap.set({"n", "v", "t"}, "<C-g>", [[<C-\><C-n>$Fll]], { noremap = true, silent = true })
+
+        vim.keymap.set({"t"}, "<C-y>", 'mzg_v$Fll"+y`za', { noremap = true, silent = true }) -- greatest term keymap
+        vim.keymap.set({"n", "v"}, "<C-y>", '<C-\\><C-n>mzg_v$Fll"+y`z', { noremap = true, silent = true }) -- greatest term keymap
     end
 }
 
