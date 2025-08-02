6b349f46ad6edd258dac521e0ae0dc62436c1910
ZT <ztthingsgm@gmail.com>
2025-08-02 22:07:46 +0700

    Added typescript to lsp ensure installed

--- lua/zt/lazy/lsp.lua
+++ lua/zt/lazy/lsp.lua
@@ -44,6 +44,7 @@ return {
             ensure_installed = {
                 "lua_ls",
                 "rust_analyzer",
+                "typescript-language-server",
                 "gopls",
             },
             handlers = {
