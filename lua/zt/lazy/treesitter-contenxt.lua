dcfd6916acc295393856bb57050d2cef238c5ec3
ZT <ztthingsgm@gmail.com>
2025-08-10 21:34:17 +0700

    Some treesitter changes

---
+++ lua/zt/lazy/treesitter-contenxt.lua
@@ -0,0 +1,13 @@
+return {
+  "nvim-treesitter/nvim-treesitter-context",
+  event = "VeryLazy",
+  config = function()
+    require("treesitter-context").setup({
+      enable = true,         -- Enable this plugin
+      max_lines = 0,         -- How many lines the context window can span
+      trim_scope = "outer",  -- Which context lines to discard if max_lines is exceeded
+      mode = "cursor",       -- Line to show context from ('cursor' or 'topline')
+    })
+  end,
+}
+
